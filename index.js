3c3068dfa4d627ae2ad0317d7f5c282e462a3e67
bitttttten <bitttttten@googlemail.com>
2019-10-01 14:18:29 +0200

    use babel jest

--- index.js
+++ index.js
@@ -3,28 +3,23 @@
 const matter = require('gray-matter')
 const stringifyObject = require('stringify-object')
 const mdx = require('@mdx-js/mdx')
-const babel = require('@babel/core')
+const { process } = require('babel-jest')
 
 function parseFrontMatter(src) {
-  const { content, data } = matter(src);
+    const { content, data } = matter(src)
 
-  const code = `export const frontMatter = ${stringifyObject(data)};${content}`;
-
-  return code;
+    return `export const frontMatter = ${stringifyObject(
+        data,
+    )};
+    
+${content}`
 }
 
-function createTransformer(src) {
+function createTransformer(src, ...rest) {
     const withFrontMatter = parseFrontMatter(src)
     const jsx = mdx.sync(withFrontMatter)
     const toTransform = `import {mdx} from '@mdx-js/react';${jsx}`
-    const babelRes = babel.transform(toTransform, {
-        presets: [require.resolve('babel-preset-react-app')],
-        babelrc: false,
-        configFile: false,
-        filename: 'null',
-    }).code
-
-    return babelRes
+    return process(toTransform, ...rest).code
 }
 
 module.exports = {
