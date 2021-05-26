d795a9f3a9eaa31136009a09274af1e4ffbfa812
bitttttten <bitttttten@googlemail.com>
2021-05-26 22:07:21 +0200

    feat: support all jest config formats

--- index.js
+++ index.js
@@ -13,19 +13,38 @@ function resolveMdxOptions(src) {
 	return src
 }
 
-function parseFrontMatter(src, options = {}) {
+function parseFrontMatter(src, frontMatterName = "frontMatterName") {
 	const { content, data } = matter(src)
-	const frontMatterName = options.frontMatterName || "frontMatter"
 
 	return `export const ${frontMatterName} = ${stringifyObject(data)};
 
 ${content}`
 }
 
+function resolveOptions(config) {
+	if (config.transformerConfig?.mdxOptions) {
+		return config.transformerConfig.mdxOptions
+	}
+
+	let options = {}
+
+	for (let i = 0; i < config.transform.length; i++) {
+		if (new RegExp(config.transform[i][0]).test(filename)) {
+			options = config.transform[i][2]
+		}
+	}
+
+	return options
+}
+
 async function createTransformer(src, filepath, config) {
-	const withFrontMatter = parseFrontMatter(src, config.transformerConfig)
-	const mdxOptions = resolveMdxOptions(config.transformerConfig?.mdxOptions)
+	const options = resolveOptions(config)
+	const mdxOptions = resolveMdxOptions(options?.mdxOptions)
+
+	const withFrontMatter = parseFrontMatter(src, options?.frontMatterName)
+
 	const jsx = await mdx(withFrontMatter, { ...mdxOptions, filepath })
+
 	const toTransform = `import {mdx} from '@mdx-js/react';${jsx}`
 
 	return babelJest.process(toTransform, filepath, config).code
