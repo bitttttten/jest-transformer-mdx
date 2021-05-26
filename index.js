c71e4f60f9b13ba721d17ab83d4fa549d17535db
bitttttten <bitttttten@googlemail.com>
2021-05-26 18:50:19 +0200

    add in prettier, and configure for jest 27

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
-'use strict'
+"use strict"
 
-const matter = require('gray-matter')
-const stringifyObject = require('stringify-object')
-const mdx = require('@mdx-js/mdx')
-const { process } = require('babel-jest')
+const matter = require("gray-matter")
+const stringifyObject = require("stringify-object")
+const mdx = require("@mdx-js/mdx")
+const { process } = require("babel-jest").default
 
 function parseFrontMatter(src, options = {}) {
-    const { content, data } = matter(src)
-    const frontMatterName = options.frontMatterName || "frontMatter"
+	const { content, data } = matter(src)
+	const frontMatterName = options.frontMatterName || "frontMatter"
 
-    return `export const ${frontMatterName} = ${stringifyObject(
-        data,
-    )};
+	return `export const ${frontMatterName} = ${stringifyObject(data)};
 
 ${content}`
 }
 
-function createTransformer(src, filename, config, transformOptions) {
-    let options = {}
+function createTransformer(src, filename, config) {
+	const withFrontMatter = parseFrontMatter(src, config.transformerConfig)
+	const jsx = mdx.sync(withFrontMatter)
+	const toTransform = `import {mdx} from '@mdx-js/react';${jsx}`
 
-    for (let i = 0; i < config.transform.length; i++) {
-        if (new RegExp(config.transform[i][0]).test(filename)) {
-            options = config.transform[i][2]
-        }
-    }
-
-    const withFrontMatter = parseFrontMatter(src, options)
-    const jsx = mdx.sync(withFrontMatter)
-    const toTransform = `import {mdx} from '@mdx-js/react';${jsx}`
-    return process(toTransform, filename, config, transformOptions).code
+	return process(toTransform, filename, config).code
 }
 
 module.exports = {
-    process: createTransformer,
+	process: createTransformer,
 }
